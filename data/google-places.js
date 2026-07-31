3755eecd3c57402d8188dee1caf6269c4e15f01f
Johnny Chin <sui2feng1@gmail.com>
2026-07-31 23:37:31 +0200

    Correct Salo property map location (#24)
    Co-authored-by: prodigalson <3868095+prodigalson@users.noreply.github.com>

--- data/google-places.js
+++ data/google-places.js
@@ -4,4 +4,9 @@ export const googlePlacesByListingId = Object.freeze({
         expectedName: 'Rocca di S.Apollinare',
         mapsUrl: 'https://www.google.com/maps/search/?api=1&query=Rocca%20di%20S.Apollinare%2C%2006055%20Marsciano%20PG%2C%20Italy',
     },
+    'jamesedition-p1-002-salo-lombardy-castle': {
+        query: 'Via Versine, 25087 Salò BS, Italy',
+        expectedName: 'Castle in Salo',
+        mapsUrl: 'https://www.google.com/maps/search/?api=1&query=Via%20Versine%2C%2025087%20Sal%C3%B2%20BS%2C%20Italy',
+    },
 });
